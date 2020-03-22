0af685c79360fbf27ec9eb8859e6c4e8d35df638
Mikalai Radomski <radomskynn@tut.by>
2020-03-22 17:51:11 +0300

    feat: add code

--- src/main/kotlin/subtask4/StringParser.kt
+++ src/main/kotlin/subtask4/StringParser.kt
@@ -2,8 +2,63 @@ package subtask4
 
 class StringParser {
 
-    // TODO: Complete the following function
+
     fun getResult(inputString: String): Array<String> {
-        throw NotImplementedError("Not implemented")
+        var charArray = charArrayOf()
+        charArray = inputString.toCharArray()
+        var outString: ArrayList<String> = arrayListOf()
+            for ((index, char) in charArray.withIndex()) {
+                if (char == '<') {
+                    var closeAngelBracket = charArray.indexOf('>')
+                    var str = inputString.substring(index + 1, closeAngelBracket)
+                    var count = str.indexOf('<')
+                    if (closeAngelBracket > 0 && count < 0) {
+                        outString.add(inputString.substring(index + 1, closeAngelBracket))
+                        charArray.set(closeAngelBracket, '-')
+                    }
+                    if (closeAngelBracket > 0 && count > 0) {
+                        charArray.set(closeAngelBracket,'-')
+                        var nextCloseAngelBracket = charArray.indexOf('>')
+                        outString.add(inputString.substring(index + 1, nextCloseAngelBracket))
+                        charArray.set(nextCloseAngelBracket, '-')
+                        charArray.set(closeAngelBracket,'>')
+                    }
+                }
+
+                if (char == '[') {
+                    var closeAngelBracket = charArray.indexOf(']')
+                    var str = inputString.substring(index + 1, closeAngelBracket)
+                    var count = str.indexOf('[')
+                    if (closeAngelBracket > 0 && count < 0) {
+                        outString.add(inputString.substring(index + 1, closeAngelBracket))
+                        charArray.set(closeAngelBracket, '-')
+                    }
+                    if (closeAngelBracket > 0 && count > 0) {
+                        charArray.set(closeAngelBracket,'-')
+                        var nextCloseAngelBracket = charArray.indexOf(']')
+                        outString.add(inputString.substring(index + 1, nextCloseAngelBracket))
+                        charArray.set(nextCloseAngelBracket, '-')
+                        charArray.set(closeAngelBracket,']')
+                    }
+                }
+                if (char == '(') {
+                    var closeAngelBracket = charArray.indexOf(')')
+                    var str = inputString.substring(index + 1, closeAngelBracket)
+                    var count = str.indexOf('(')
+                    if (closeAngelBracket > 0 && count < 0) {
+                        outString.add(inputString.substring(index + 1, closeAngelBracket))
+                        charArray.set(closeAngelBracket, '-')
+                    }
+                    if (closeAngelBracket > 0 && count > 0) {
+                        charArray.set(closeAngelBracket,'-')
+                        var nextCloseAngelBracket = charArray.indexOf(')')
+                        outString.add(inputString.substring(index + 1, nextCloseAngelBracket))
+                        charArray.set(nextCloseAngelBracket, '-')
+                        charArray.set(closeAngelBracket,')')
+                    }
+                }
+
+            }
+        return outString.toTypedArray()
     }
 }
