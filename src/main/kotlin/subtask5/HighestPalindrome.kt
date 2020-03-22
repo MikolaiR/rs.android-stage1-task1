197b429dc3a903743cb47ab4439f19828ffeef48
Mikalai Radomski <radomskynn@tut.by>
2020-03-22 17:51:29 +0300

    feat: add code

--- src/main/kotlin/subtask5/HighestPalindrome.kt
+++ src/main/kotlin/subtask5/HighestPalindrome.kt
@@ -2,8 +2,48 @@ package subtask5
 
 class HighestPalindrome {
 
-    // TODO: Complete the following function
+    fun stringToInt(numberString: String): ArrayList<Int> {
+        var outArray = arrayListOf<Int>()
+        var count = 0
+        var numberASCI: Int = 0
+        while (count < numberString.length) {
+            if (numberString[count].isDigit()) {
+                numberASCI = numberString[count].toInt()
+                outArray.add(numberASCI - 48)
+            } else "Pleas enter to number"
+            count++
+        }
+        return outArray
+    }
+
     fun highestValuePalindrome(n: Int, k: Int, digitString: String): String {
-        throw NotImplementedError("Not implemented")
+        var count = 0
+        var output = ""
+        var invertNumber = mutableListOf<Int>()
+        var originNumber = mutableListOf<Int>()
+        originNumber = stringToInt(digitString)
+        invertNumber = originNumber.reversed().toMutableList()
+        var maxValue = originNumber.max()
+
+        while (count <= k) {
+            for (index in 0 until invertNumber.size) {
+                if (invertNumber[index] != originNumber[index] && invertNumber[index] > originNumber[index]) {
+                    if (maxValue != null) {
+                        originNumber[index] = maxValue
+                        invertNumber = originNumber.reversed().toMutableList()
+                        break
+                    }
+                } else if (originNumber == invertNumber) {
+                    break
+                }
+            }
+            count++
+        }
+        if (originNumber != invertNumber) {
+            return "-1"
+        }else {
+            originNumber.forEach{output += it }
+        }
+        return output
     }
 }
