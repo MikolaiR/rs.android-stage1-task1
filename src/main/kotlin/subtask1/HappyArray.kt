b5b5d1c7f9b9b632eb1c93f03adb154b7efbf4a1
Mikalai Radomski <radomskynn@tut.by>
2020-03-22 17:50:28 +0300

    feat: add code

--- src/main/kotlin/subtask1/HappyArray.kt
+++ src/main/kotlin/subtask1/HappyArray.kt
@@ -2,8 +2,19 @@ package subtask1
 
 class HappyArray {
 
-    // TODO: Complete the following function
     fun convertToHappy(sadArray: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var outArray = arrayListOf<Int>()
+        sadArray.forEach { outArray.add(it) }
+        var counte: Int = 1
+        while (counte < outArray.size - 1) {
+            if (outArray[counte] > outArray.get(counte - 1) + outArray.get(counte + 1)) {
+                outArray.remove(outArray[counte])
+                counte = 1
+            }
+            counte++
+        }
+        return outArray.toIntArray()
     }
 }
+
+
