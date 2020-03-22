6d408297e7ecc3ad3714f2a34b34e157864fbaed
Mikalai Radomski <radomskynn@tut.by>
2020-03-22 17:50:41 +0300

    feat: add code

--- src/main/kotlin/subtask2/MiniMaxSum.kt
+++ src/main/kotlin/subtask2/MiniMaxSum.kt
@@ -2,8 +2,19 @@ package subtask2
 
 class MiniMaxSum {
 
-    // TODO: Complete the following function
     fun getResult(input: IntArray): IntArray {
-        throw NotImplementedError("Not implemented")
+        var count: Int = 0
+        var output: ArrayList<Int> = arrayListOf<Int>(0, 0)
+        while (count < input.size) {
+            if (output[0] > input.sum() - input[count] || output[0] == 0) {
+                output[0] = input.sum() - input[count]
+                println(output.get(0))
+            }
+            if (output[1] < input.sum() - input[count]) {
+                output[1] = input.sum() - input[count]
+            }
+            count++
+        }
+        return output.toIntArray()
     }
 }
